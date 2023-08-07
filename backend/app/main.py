2a2fa2d3a1217e3ccd42df229d1362576e1f3abd
meizi9 <1723680221@qq.com>
2023-08-07 22:47:41 +0800

    主要

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 import random
+import aiofiles
 from fastapi import FastAPI, HTTPException
 from database.redis_db import redis_db_yibu
 from pydantic import BaseModel
@@ -14,8 +15,8 @@ class PoetryItem(BaseModel):
 
 async def get_random_name():
     filename = 'name.txt'
-    with open(filename, 'r', encoding='utf-8') as f:
-        names = f.readlines()
+    async with aiofiles.open(filename, 'r', encoding='utf-8') as f:
+        names = await f.readlines()
         name = random.choice(names)
         return name.strip()
 
@@ -26,9 +27,13 @@ async def add_poetry(poetry_item: PoetryItem):
     attempts = 0
     while attempts < max_attempts:
         name = await get_random_name()
-        if not redis_db_yibu.exists(name):
-            redis_db_yibu.rpush(name, poetry_item.model_dump_json())
-            return {'message': 'add successfully'}
+        a = await redis_db_yibu.exists(name)
+        if not a:
+            # 加await后添加成功 看来跟异步有关
+            await redis_db_yibu.set(name, poetry_item.model_dump_json())
+            print(name)
+            print(poetry_item.model_dump_json())
+            return {'message': f'add {name} successfully'}
         attempts += 1
     raise HTTPException(status_code=400, detail='Failed to add. Name existed.')
 
@@ -54,12 +59,22 @@ async def get_random_poetry():
     name = pipeline.randomkey()
     pipeline.get(name)
     result = pipeline.execute()
-    name = result[1]
-    value = result[2]
+    name = result[0]
+    value = result[1]
     return {'name': name, 'value': value}
 
 
-@app.get('/poetry/get')
+@app.get('/poetry/all/get')
+async def get_all_poetry():
+    size = redis_db_yibu.dbsize()
+    if size == 0:
+        return {'message': 'Database is empty.'}
+    keys = await redis_db_yibu.keys('*')
+    values = await redis_db_yibu.mget(keys)
+    response = [{'name': key, 'value': value} for key, value in zip(keys, values)]
+
+    return response
+@app.get('/poetry/name/get')
 async def get_poetry_by_name(name):
     pipeline = redis_db_yibu.pipeline()
     pipeline.get(name)
@@ -72,12 +87,15 @@ async def get_poetry_by_name(name):
 
 @app.get('/create_md/{name}')
 async def create_md_file(name):
-    value = await redis_db_yibu.get(name)
+    value = redis_db_yibu.get(name)
     if not value:
         raise HTTPException(status_code=404, detail='Key does not exist in the database.')
 
     filename = f'Md/{name}.md'
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(value.decode('utf-8'))
-
     return {'message': 'MD file created successfully.'}
+
+
+if __name__ == '__main__':
+    pass
