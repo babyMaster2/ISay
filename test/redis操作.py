6ba3aa8a5872054fe6c1d9705e4e8e916a2b3e0b
meizi9 <1723680221@qq.com>
2023-08-15 22:26:03 +0800

    测试

--- test/redis操作.py
+++ test/redis操作.py
@@ -1,7 +1,13 @@
 from src.async_redis_client import async_redis_client
 
 redis_obj = async_redis_client
+async def add():
+    res = await redis_obj.hset(name='静夜思', mapping={'作者': '李白', '内容前两句': '床前明月光，疑是地上霜。', '内容后两句': '举头望明月，低头思故乡。'})
+    return res
 
-res = redis_obj.hset('静夜思', {'作者': '李白'}, {'内容前两句': '床前明月光，疑是地上霜。'}, {'内容后两句': '举头望明月，低头思故乡。'})
+
+res = add()
 print(res)
+
+
 
